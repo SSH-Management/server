5d128a8f483fd413eec75d5052b408b451e1a88b
Dusan Malusev <dmalusev@nanointeractive.com>
2021-12-06 20:37:05 +0100

    Error handling added to client grpc server
    Signed-off-by: Dusan Malusev <dmalusev@nanointeractive.com>

--- pkg/services/grpc/client/client.go
+++ pkg/services/grpc/client/client.go
@@ -5,6 +5,10 @@ import (
 
 	"github.com/SSH-Management/protobuf/common"
 	"github.com/SSH-Management/protobuf/server/clients"
+	"github.com/go-playground/validator/v10"
+	"github.com/leebenson/conform"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 
 	"github.com/SSH-Management/server/pkg/db"
@@ -21,44 +25,64 @@ type ClientService struct {
 	logger           *log.Logger
 	serverRepository server.Interface
 	userRepository   userrepo.Interface
+	validator        *validator.Validate
 
 	serverPublicKey string
 }
 
-func New() *ClientService {
-	return &ClientService{}
+func New(
+	serverPublicKey string,
+	logger *log.Logger,
+	serverRepository server.Interface,
+	userRepository userrepo.Interface,
+	validator *validator.Validate,
+) *ClientService {
+	return &ClientService{
+		serverPublicKey:  serverPublicKey,
+		logger:           logger,
+		serverRepository: serverRepository,
+		userRepository:   userRepository,
+		validator:        validator,
+	}
 }
 
 func (cl *ClientService) Create(ctx context.Context, req *clients.CreateClientRequest) (*clients.CreateClientResponse, error) {
+	if err := conform.Strings(&req); err != nil {
+		return nil, err
+	}
+
+	if err := cl.validator.Struct(req); err != nil {
+		return nil, err
+	}
+
 	s, err := cl.serverRepository.FindByPrivateIP(ctx, req.Ip)
+
 	if err != nil {
 		if err == db.ErrNotFound {
 			s, err = cl.serverRepository.Create(ctx, req)
 
 			if err != nil {
-				// return c.Status(http.StatusInternalServerError).JSON(ErrorResponse{
-				// 	Message: "Error while creating server",
-				// })
+				return nil, status.Error(codes.Internal, "Error while creating server")
 			}
+
 		} else {
-			// return c.Status(http.StatusInternalServerError).JSON(ErrorResponse{
-			// 	Message: "Error while creating server",
-			// })
+			return nil, status.Error(codes.Internal, "Error while creating server")
 		}
 	}
 
 	users, err := cl.userRepository.FindByGroup(ctx, s.GroupID)
+
 	if err != nil {
 		err = cl.serverRepository.Delete(ctx, s.ID)
+
 		if err != nil {
 			cl.logger.
 				Error().
 				Err(err).
 				Msg("Error while deleting server")
 		}
-		// return c.Status(http.StatusInternalServerError).JSON(ErrorResponse{
-		// 	Message: "Error while creating server",
-		// })
+
+		return nil, status.Error(codes.Internal, "Error while creating server")
 	}
 
 	return &clients.CreateClientResponse{
@@ -70,11 +94,11 @@ func (cl *ClientService) Create(ctx context.Context, req *clients.CreateClientRe
 
 func (cl *ClientService) Delete(ctx context.Context, req *clients.DeleteClientRequest) (*emptypb.Empty, error) {
 	err := cl.serverRepository.Delete(ctx, req.Id)
+
 	if err != nil {
-		// return ctx.Status(http.StatusInternalServerError).JSON(ErrorResponse{
-		// 	Message: "Error while creating server",
-		// })
+		return nil, status.Error(codes.Internal, "Error while deleting server")
 	}
+
 	return &emptypb.Empty{}, nil
 }
 
